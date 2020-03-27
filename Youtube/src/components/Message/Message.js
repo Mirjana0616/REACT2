673283ffd796a922c79d43b332c6eff358fa773a
Mirjana0616 <mikamikamata@gmail.com>
2020-03-27 16:41:13 +0100

    r2

---
+++ Youtube/src/components/Message/Message.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Message.scss';
+
+
+const Message = () => {
+
+		return (
+			<section className="message">
+				<h1>Welcome!!!</h1>
+			</section>
+
+		)
+}
+
+export {Message};
